c8f7a1d3402e728bf4f73ceafdc0f84f97bfa48f
Abhinav Parab <abhinav@reldyn.co>
2024-01-10 22:09:44 +0530

    Initial commit

---
+++ src/main/java/co/reldyn/baas/rd/baas/jenkins/deployment/sample/RdBaasJenkinsDeploymentSampleApplication.java
@@ -0,0 +1,13 @@
+package co.reldyn.baas.rd.baas.jenkins.deployment.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdBaasJenkinsDeploymentSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdBaasJenkinsDeploymentSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/reldyn/baas/rd/baas/jenkins/deployment/sample/RdBaasJenkinsDeploymentSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package co.reldyn.baas.rd.baas.jenkins.deployment.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdBaasJenkinsDeploymentSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
